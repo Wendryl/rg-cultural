ae8d8fa59eaa0140cb2d6e402d6dada370810ef3
Wendryl <wendryl10000@gmail.com>
2022-04-13 19:32:32 -0300

    Move front-end depedency to npm

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cadastro</title>
-    <link rel="stylesheet" href="vendor/twbs/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="resources/css/style.css">
   </head>
   <body>
@@ -58,8 +58,8 @@
         </div>
       </div>
     </div>
-    <script src="vendor/components/jquery/jquery.slim.min.js"></script>
-    <script src="vendor/twbs/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script src="node_modules/jquery/dist/jquery.slim.min.js"></script>
+    <script src="node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
     <script src="resources/js/formulario.js"></script>
   </body>
 </html>
