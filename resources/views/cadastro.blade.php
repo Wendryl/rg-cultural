27d76e6599d769cc32e7e5359de9cb14d3a5049a
Wendryl <wendryl10000@gmail.com>
2022-06-16 21:56:55 -0300

    Fix login and register forms styling

--- resources/views/cadastro.blade.php
+++ resources/views/cadastro.blade.php
@@ -1,23 +1,53 @@
-@extends('templateIndex_head');
+@extends('templateIndex_head'); @section('head')
+<title>TV Taubaté | Registrar</title>
+@endsection @section('body')
+<form class="form__cadastro" onsubmit="register(event)">
+    <h1 class="form__cadastro-titulo">Cadastrar</h1>
 
-@section('head')
-    <title>TV Taubaté | Registrar</title>
-@endsection
+    <h2 class="form__cadastro-email">Email (Também será o seu ID):</h2>
+    <input
+        class="form__cadastro-input"
+        type="email"
+        minlength="5"
+        name="email"
+        required
+        placeholder="exemplo@gmail.com"
+    /><br /><br />
 
-@section('body')
-    <form class='form__registro' onsubmit="register(event)">
-        <h1 class='form__registro-titulo'>Cadastrar</h1>
-        <label class="label" for='useremail'>Email (também será seu ID):</label> <br>
-        <input class='form__registro-input' type="email" name="email" placeholder="seuEmail@gmail.com" required><br><br>
-        <label class="label" for="username">Usuario:</label> <br>
-        <input class='form__registro-input' type="text" minlength="5" name="name" required placeholder="Seu usuario"><br><br>
-        <label class="label" for="password">Senha (pelo menos 8 caracteres):</label> <br>
-        <input class='form__registro-input' type="password" minlength="8" name="password" required placeholder="********">
-        <p class='form__registro-link'><a href=" {{ url('login') }}" > Já possui conta?</a></p>
-        <input type="submit" value="Cadastrar" class='form__registro-botao'>
-        <input type="reset" value="Limpar" class='form__registro-botao'>
-    </form>
-    @push('scripts')
-        <script src="{{ asset('js/register.js') }}"></script>
-    @endpush
-@endsection
+    <h2 class="form__cadastro-user">Usuário:</h2>
+    <input
+        class="form__cadastro-input"
+        type="name"
+        minlength="5"
+        name="name"
+        required
+        placeholder="Seu Usuário"
+        data-form-senha
+    /><br /><br />
+
+    <h2 class="form__cadastro-pass">Senha:</h2>
+    <input
+        class="form__cadastro-input"
+        type="password"
+        minlength="5"
+        name="password"
+        required
+        placeholder="********"
+        data-form-confirma-senha
+    /><br /><br />
+
+    <div class="container_senha-errada"></div>
+
+    <p class="form__cadastro-link">
+        <a href="{{ url('login') }}"> Já possui conta?</a>
+    </p>
+
+    <div class="botoes__acoes">
+        <input type="submit" value="Cadastrar" class="form__registro-botao" />
+        <input type="reset" value="Limpar" class="form__registro-botao" />
+    </div>
+</form>
+
+@push('scripts')
+<script src="{{ asset('js/register.js') }}"></script>
+@endpush @endsection
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,20 +1,35 @@
-@extends('templateIndex_head')
+@extends('templateIndex_head') @section('head')
+<title>TV Taubaté | Login</title>
+@endsection @section('body')
+<form class="form__registro" onsubmit="login(event)">
+    <h1 class="form__registro-titulo">Entrar</h1>
+    <h2 class="form__registro-user">Usuario:</h2>
+    <input
+        class="form__registro-input-user"
+        type="email"
+        name="email"
+        placeholder="Email"
+        required
+    />
+    <h2 class="form__registro-pass">Senha:</h2>
+    <input
+        class="form__registro-input-pass"
+        type="password"
+        name="password"
+        placeholder="Senha"
+        required
+    />
+    <div class="links__form">
+        <p class="form__registro-link">
+            <a href="{{ url('esqueci_senha') }}">esqueci minha senha</a>
+        </p>
 
-@section('head')
-    <title>TV Taubaté | Login</title>
-@endsection
-@section('body')
-    <form class='form__registro' onsubmit="login(event)">
-        <h1 class='form__registro-titulo'>Entrar</h1>
-        <label class="label" for='id'>Usuario:</label> <br>
-        <input class='form__registro-input' type="email" name="email" placeholder="seuEmaul@gmail.com" required><br><br>
-        <label class="label" for='password'>Senha:</label> <br>
-        <input class='form__registro-input' type="password" name="password" placeholder="Insira tua senha" required>
-        <p class='form__registro-link'><a href=" {{ url('esqueci_senha') }} " >esqueci minha senha</a></p>
-        <p class='form__registro-link'><a  href="{{ url('registrar') }}">Cadastrar</a></p>
-        <button class='form__registro-botao'  type="submit" >Entrar</button>
-    </form>
-    @push('scripts')
-        <script src="{{ asset('js/login.js') }}"></script>
-    @endpush
-@endsection
+        <p class="form__registro-link">
+            <a href="{{ url('registrar') }}">Cadastrar</a>
+        </p>
+    </div>
+    <button class="form__registro-botao" type="submit">Entrar</button>
+</form>
+@push('scripts')
+<script src="{{ asset('js/login.js') }}"></script>
+@endpush @endsection
