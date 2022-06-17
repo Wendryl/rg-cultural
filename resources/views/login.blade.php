db94abbe7669c6a5d781675c9a08fbf19f704ac1
Wendryl <wendryl10000@gmail.com>
2022-06-16 22:19:12 -0300

    Fix login styling

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,34 +1,34 @@
 @extends('templateIndex_head') @section('head')
 <title>TV Taubaté | Login</title>
 @endsection @section('body')
-<form class="form__registro" onsubmit="login(event)">
-    <h1 class="form__registro-titulo">Entrar</h1>
-    <h2 class="form__registro-user">Usuario:</h2>
+<form class="form__login" onsubmit="login(event)">
+    <h1 class="form__login-titulo">Entrar</h1>
+    <h2 class="form__login-user">Usuario:</h2>
     <input
-        class="form__registro-input-user"
+        class="form__login-input-user"
         type="email"
         name="email"
         placeholder="Email"
         required
     />
-    <h2 class="form__registro-pass">Senha:</h2>
+    <h2 class="form__login-pass">Senha:</h2>
     <input
-        class="form__registro-input-pass"
+        class="form__login-input-pass"
         type="password"
         name="password"
         placeholder="Senha"
         required
     />
     <div class="links__form">
-        <p class="form__registro-link">
+        <p class="form__login-link">
             <a href="{{ url('esqueci_senha') }}">esqueci minha senha</a>
         </p>
 
-        <p class="form__registro-link">
+        <p class="form__login-link">
             <a href="{{ url('registrar') }}">Cadastrar</a>
         </p>
     </div>
-    <button class="form__registro-botao" type="submit">Entrar</button>
+    <button class="form__login-botao" type="submit">Entrar</button>
 </form>
 @push('scripts')
 <script src="{{ asset('js/login.js') }}"></script>
