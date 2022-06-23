0113ae7f19f9f81d4ec5f79d2dbb2dec76d2873d
Wendryl <wendryl10000@gmail.com>
2022-06-23 08:58:27 -0300

    Add 'sobre nos'

--- resources/views/sobre_nos.blade.php
+++ resources/views/sobre_nos.blade.php
@@ -1,6 +1,115 @@
-@extends('templateIndex_head');
-@section('head')
-    <title> Sobre Nós</title>
-@endsection
-@section('body')
+@extends('templateIndex_head'); @section('head')
+<title>Sobre Nós</title>
+@endsection @section('body')
+<div class="card">
+    <h1>Equipe FATEC</h1>
+    <h4>Kaio Santos</h4>
+    <p>
+        Estudante de Análise e Desenvolvimento de Sistemas desde 2019 à 2022 e
+        trabalhando como estágiário na área. Com expêriencia em automação de
+        processos e administração de servidores.
+    </p>
+    <h4>Lucas Rodrigo</h4>
+    <p>
+        Estudante de Analise e Desenvolvimento de Sistemas, com conhecimento em
+        HTML, CSS, C e C++. Formado na area de tecnico em automação industrial,
+        ja fiz cursos exteriores como por exemplo: Figma, UML, etc.
+    </p>
+    <h4>Wendryl Santos</h4>
+    <p>
+        Desenvolvedor de Sistemas e estudante de Análise e Desenvolvimento de
+        Sistemas na Faculdade de Tecnologia de Taubaté. Possui experiência no
+        desenvolvimento de sistemas web arquitetura MVC, REST API's e Single
+        Page Applications. Atualmente trabalha como desenvolvedor Front-end na
+        empresa Lean Tech e é responsável pelo desenvolvimento de API e
+        integração do projeto RG Cultural.
+    </p>
+    <h4>Professor Ronaldo Emerick Moreira</h4>
+    <p>
+        Graduação em Informática pela Universidade de Taubaté (1991) , pós em
+        gestão de TI pelo ITA , pós Lato Sensu em Administração financeira e
+        Auditoria, Pós-graduação Lato Sensu em psicopedagogia Institucional e
+        Clínica, Pós-graduação Lato Sensu em coordenação Pedagógica e Supervisão
+        escolar e mestrado em Engenharia, doutorado incompleto em Engenharia de
+        Dados. É professor de MBA da PUC-Minas em Poços de Caldas e MBA da FIAP
+        em Administração, palestrante de Governança corporativa e governança de
+        TI, consultor da UNESCO. Com experiência na área de administração de TI
+        com PMI e ITIL. Atuou como coordenador do curso de Ciências da
+        Computação na Anhanguera de São Jose dos Campos. Professor de Ensino
+        Superior FATEC de Tecnologia de Taubaté e em Administração na FATEC
+        EAD-CPS.
+    </p>
+    <h4>Professora Divani Barbosa Gavinier</h4>
+    <p>
+        Possui graduação em Computação Cientifica pela Universidade de Taubaté
+        (2003), mestrado (2005), doutorado (2009) e dois pós-doutorados (2014 e
+        2016) em Engenharia e Tecnologia Espaciais pelo Instituto Nacional de
+        Pesquisas Espaciais. Atualmente é professora de nível superior e
+        coordenadora do curso de Análise e Desenvolvimento de Sistemas na Fatec
+        Taubaté. Tem experiência na área de simulação numérica computacional e
+        deposição de filmes finos pela técnica de deposição química a-partir da
+        fase vapor.
+    </p>
+    <h4>Professor Flávio Groh</h4>
+    <p>
+        Possui graduação em Engenharia Mecânica pela Universidade de Taubaté
+        (1986) pós graduação em Engenharia de Segurança pela Universidade de
+        Taubaté (1990), MBA em Consultoria Empresarial pela Faculdade de
+        Tecnologia SENAI de Florianópolis (2011) e mestrado em Engenharia
+        Mecânica pela Universidade de Taubaté (2003), doutorado interrompido
+        pela Unesp Guaratinguetá. Professor de Ensino Superior III A e
+        Coordenador de Curso na Fatec Taubaté. Consultor de empresas na área de
+        desenvolvimento organizacional ampla experiência em estruturação de
+        programas de capacitação profissional. Possui experiência na área da
+        educação superior. Perito Judicial nas áreas de mecânica e de segurança
+        no trabalho.
+    </p>
+    <h4>Professor e Diretor Claudemir Stelatti</h4>
+    <p>
+        Possui graduação em Licenciatura Plena Em Física pela Universidade de
+        Taubaté (1991), Mestrado em Física pela Universidade Estadual Paulista
+        Júlio de Mesquita Filho (1994) e Doutorado em Física pelo Instituto
+        Tecnológico de Aeronáutica (2006). Atualmente é Professor Assistente
+        Doutor da Universidade de Taubaté, Diretor da instituição - Fatec de
+        Tecnologia de Taubaté e Diretor e Professor Pleno I - Tem experiência na
+        área de Física, atuando principalmente nos seguintes temas: Detector
+        Mário Schenberg, Ondas Gravitacionais, Gravitação, Automação e Labview e
+        Desenvolvimento de Sensores.
+    </p>
+    <h1>Equipe TV Taubaté</h1>
+    <h4>Luiz Claudio Daniel</h4>
+    <p>
+        Pedagogo, arte-educador, jornalista, ator, diretor, iluminador,
+        cenógrafo repórter, produtor e comentarista de televisão e cinema,
+        vice-presidente da Rede Cidade de Comunicação e Cidadania/TV Cidade
+        Taubaté, diretor-geral de diversos filmes e peças para teatro, dentro os
+        quais o curta "O Caipira e a Bicicleta", diretor cinematográfico com
+        registro na ANCINE nº 25.914. Gestor cultural de eventos com dezenas de
+        realizações no interior paulista, cursos de extensão C.T.A.V. Rio de
+        Janeiro. Membro do Conselho Municipal de Cultura de Taubaté (2020/2021),
+        na cadeira de artes visuais e design
+    </p>
+    <h4>Cláudia Valéria de Castro Perroni Leite Melo</h4>
+    <p>
+        Brasileira, jornalista, cinegrafista, roteirista, documentarista e
+        produtora de audiovisual. Atua na OSCIP Rede Cidade de Comunicação e
+        Cidadania (TV Cidade Taubaté), como gestora de projetos especiais,
+        ministrando oficinas de produção audiovisual para adolescentes. Registro
+        na ANCINE nº 25.791. Formada em cursos técnicos de extensão para cinema
+        no C.T.A.V./ Rio de Janeiro. Membro fundadora do Conselho Municipal de
+        Cultura de Taubaté (2018/2019), na cadeira de audiovisual.
+    </p>
+    <h4>Mário Jéfferson Leite Melo</h4>
+    <p>
+        Consultor organizacional para terceiro setor, diretor-presidente da Rede
+        Cidade de Comunicação e Cidadania (TV Cidade Taubaté),
+        diretor-presidente da FRENAVATEC (Frente Nacional pela Valorização das
+        TVs do Campo Público), ouvidor e conselheiro da TV Câmara de Taubaté,
+        jornalista, radialista profissional, coordenador do Pontão de Cultura
+        Central de Mídia Produção e Distribuição Cultural, Pontos de Cultura
+        Fábrica de Documentários e Escola de Mídia Comunitária. Membro do
+        Conselho Municipal de Cultura de Taubaté (2020/2021), na cadeira de
+        entidades não-governamentais.
+    </p>
+</div>
 @endsection
