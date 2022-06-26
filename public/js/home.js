f1caa5b6300febad7598aa8bc93047531c3fa4a3
Wendryl <wendryl10000@gmail.com>
2022-06-26 09:38:53 -0300

    Update webpack files

--- public/js/home.js
+++ public/js/home.js
@@ -6,8 +6,15 @@ var __webpack_exports__ = {};
 _verifyUser();
 
 window.onload = function () {
+  var _user$profile_picture;
+
   var title = document.querySelector('#title');
-  if (title != null) title.innerHTML = JSON.parse(sessionStorage.getItem('user_data')).name;
+  var welcomeMessage = document.querySelector('#welcome-text');
+  var profilePic = document.querySelector('#profile-pic');
+  var user = JSON.parse(sessionStorage.getItem('user_data'));
+  if (title != null) title.innerHTML = user.name;
+  if (profilePic != null) profilePic.setAttribute('src', (_user$profile_picture = user.profile_picture) !== null && _user$profile_picture !== void 0 ? _user$profile_picture : "".concat(window.location.origin, "/img/profile.png"));
+  if (welcomeMessage != null) welcomeMessage.innerHTML = "Ol\xE1 <strong>".concat(user.name, "</strong> bem vindo ao portal <strong>RG Cultural</strong>.");
 };
 
 window.logout = function () {
@@ -46,9 +53,9 @@ window.listUsers = function () {
   }).then(function (res) {
     var listEl = document.querySelector('#list');
     res.forEach(function (user) {
-      var _user$profile_picture, _user$phone;
+      var _user$profile_picture2, _user$phone;
 
-      var template = "\n          <tr>\n            <td><img class=\"profile-thumb\" src=\"".concat((_user$profile_picture = user.profile_picture) !== null && _user$profile_picture !== void 0 ? _user$profile_picture : '/img/profile.png', "\"></img></td>\n            <td>").concat(user.name, "</td>\n            <td>").concat(user.email, "</td>\n            <td>").concat(_address(user), "</td>\n            <td>").concat((_user$phone = user.phone) !== null && _user$phone !== void 0 ? _user$phone : '-', "</td>\n            <td>\n                <a href=\"#editEmployeeModal\" class=\"edit\" data-toggle=\"modal\"><i class=\"material-icons\" data-toggle=\"tooltip\" title=\"Edit\">&#xE254;</i></a>\n                <a href=\"#deleteEmployeeModal\" class=\"delete\" data-toggle=\"modal\"><i class=\"material-icons\" data-toggle=\"tooltip\" title=\"Delete\">&#xE872;</i></a>\n            </td>\n          </tr>\n          ");
+      var template = "\n          <tr>\n            <td><img class=\"profile-thumb\" src=\"".concat((_user$profile_picture2 = user.profile_picture) !== null && _user$profile_picture2 !== void 0 ? _user$profile_picture2 : '/img/profile.png', "\"></img></td>\n            <td>").concat(user.name, "</td>\n            <td>").concat(user.email, "</td>\n            <td>").concat(_address(user), "</td>\n            <td>").concat((_user$phone = user.phone) !== null && _user$phone !== void 0 ? _user$phone : '-', "</td>\n            <td>\n                <a href=\"#editEmployeeModal\" class=\"edit\" data-toggle=\"modal\"><i class=\"material-icons\" data-toggle=\"tooltip\" title=\"Edit\">&#xE254;</i></a>\n                <a href=\"#deleteEmployeeModal\" class=\"delete\" data-toggle=\"modal\"><i class=\"material-icons\" data-toggle=\"tooltip\" title=\"Delete\">&#xE872;</i></a>\n            </td>\n          </tr>\n          ");
       var tr = document.createElement('tr');
       tr.innerHTML = template;
       listEl.appendChild(tr);
