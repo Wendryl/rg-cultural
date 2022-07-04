f2073f031d66c8965893a849b69ca2d9fb467039
Wendryl <wendryl10000@gmail.com>
2022-07-04 16:08:55 -0300

    Setup profile pic preview

--- resources/js/complete-registration.js
+++ resources/js/complete-registration.js
@@ -1,13 +1,22 @@
 _setFormData();
 
 function _setFormData() {
-  const user = JSON.parse(sessionStorage.getItem('user_data'));
-  const fields = document.querySelectorAll('input');
+    const user = JSON.parse(sessionStorage.getItem("user_data"));
+    const fields = document.querySelectorAll("input");
 
-  fields.forEach(f => {
-    if (user.hasOwnProperty(f.name))
-      f.value = user[f.name];
-  });
+    fields.forEach((f) => {
+        if (user.hasOwnProperty(f.name)) f.value = user[f.name];
+    });
 }
 
+window.openFileInput = () => {
+    const fileField = document.querySelector('input[type="file"]');
+    fileField.click();
+};
 
+window.setPicPreview = (e) => {
+    const img = document.querySelector("#pic-preview");
+    const [file] = e.target.files;
+
+    if (file) img.src = URL.createObjectURL(file);
+};
--- resources/views/complete-registration.blade.php
+++ resources/views/complete-registration.blade.php
@@ -7,13 +7,13 @@
 @endpush
 @section('body')
 <form class="form-card">
-  <div class="edit-picture">
-    <img src="{{ asset('img/profile.png')}}" alt="Sua imagem de perfil.">
+  <div class="edit-picture" onclick="openFileInput()">
+    <img src="{{ asset('img/profile.png')}}" alt="Sua imagem de perfil." id="pic-preview">
     <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="#fff" stroke-width="2">
       <path stroke-linecap="round" stroke-linejoin="round" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
     </svg>
   </div>
-  <input type="file" name="profilePicture">
+  <input type="file" name="profilePicture" onchange="setPicPreview(event)" accept=".jpg, .jpeg, .png">
   <div>
     <input type="text" name="name" placeholder="Nome*">
   </div>
