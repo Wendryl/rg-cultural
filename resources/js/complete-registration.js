04f8d6b85f7bb73ebb0f0d65c1d99e1af1dfd539
Wendryl <wendryl10000@gmail.com>
2022-07-04 16:44:39 -0300

    Setup user img upload

--- resources/js/complete-registration.js
+++ resources/js/complete-registration.js
@@ -1,22 +1,38 @@
 _setFormData();
 
 function _setFormData() {
-    const user = JSON.parse(sessionStorage.getItem("user_data"));
-    const fields = document.querySelectorAll("input");
+  const user = JSON.parse(sessionStorage.getItem("user_data"));
+  const fields = document.querySelectorAll("input");
+  const img = document.querySelector("#pic-preview");
 
-    fields.forEach((f) => {
-        if (user.hasOwnProperty(f.name)) f.value = user[f.name];
-    });
+  img.src = '/' + user.profile_picture ?? '/img/profile.png';
+
+  fields.forEach((f) => {
+    if (user.hasOwnProperty(f.name)) f.value = user[f.name];
+  });
 }
 
 window.openFileInput = () => {
-    const fileField = document.querySelector('input[type="file"]');
-    fileField.click();
+  const fileField = document.querySelector('input[type="file"]');
+  fileField.click();
 };
 
-window.setPicPreview = (e) => {
-    const img = document.querySelector("#pic-preview");
-    const [file] = e.target.files;
+window.setProfilePic = (e) => {
+  const img = document.querySelector("#pic-preview");
+  const [file] = e.target.files;
+  const userId = JSON.parse(sessionStorage.getItem("user_data")).id;
 
-    if (file) img.src = URL.createObjectURL(file);
+  if (file) img.src = URL.createObjectURL(file);
+
+  const body = new FormData();
+  body.append('profile_picture', file);
+
+  fetch(`/api/users/${userId}/profile-picture`, {
+    headers: {
+      'Authorization': `Bearer ${sessionStorage.getItem('tkn')}`
+    },
+    method: 'POST',
+    body
+  }).then(res => res.json())
+  .then(result => console.log(result))
 };
--- resources/views/complete-registration.blade.php
+++ resources/views/complete-registration.blade.php
@@ -13,7 +13,7 @@
       <path stroke-linecap="round" stroke-linejoin="round" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
     </svg>
   </div>
-  <input type="file" name="profilePicture" onchange="setPicPreview(event)" accept=".jpg, .jpeg, .png">
+  <input type="file" name="profilePicture" onchange="setProfilePic(event)" accept=".jpg, .jpeg, .png">
   <div>
     <input type="text" name="name" placeholder="Nome*">
   </div>
