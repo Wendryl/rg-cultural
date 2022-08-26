8d369512b088f66d7cbf1f9ee17b496169305d20
Wendryl <wendryl10000@gmail.com>
2022-08-26 20:14:27 -0300

    Add restriction to avoid admin users deletion

--- resources/views/admin-dashboard/index.blade.php
+++ resources/views/admin-dashboard/index.blade.php
@@ -37,6 +37,9 @@
         </thead>
         <tbody id="list">
           @foreach($users as $user)
+            @if ($user->id == Auth::user()->id)
+              @break
+            @endif
             <tr>
               <td>{{ $user->id }}</td>
               <td>
@@ -65,7 +68,7 @@
                       <path fill-rule="evenodd" d="M5.433 13.916l1.262-3.154a4 4 0 01.885-1.343L14.5 2.5a2.121 2.121 0 113 3l-6.92 6.919c-.383.383-.84.684-1.343.885l-3.154 1.262a.5.5 0 01-.65-.65zM2.5 5.75c0-.69.56-1.25 1.25-1.25H10A.75.75 0 0010 3H3.75A2.75 2.75 0 001 5.75v10.5A2.75 2.75 0 003.75 19h10.5A2.75 2.75 0 0017 16.25V10a.75.75 0 00-1.5 0v6.25c0 .69-.56 1.25-1.25 1.25H3.75c-.69 0-1.25-.56-1.25-1.25V5.75z" clip-rule="evenodd" />
                     </svg>
                   </button>
-                  <button class="ms-1 btn btn-sm btn-danger" data-toggle="modal" data-target="#deleteEmployeeModal">
+                  <button class="ms-1 btn btn-sm btn-danger" data-toggle="modal" data-target="#deleteEmployeeModal" {{ $user->type == 1 ? 'disabled': '' }}>
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
                       <path fill-rule="evenodd" d="M8.75 1A2.75 2.75 0 006 3.75v.443c-.795.077-1.584.176-2.365.298a.75.75 0 10.23 1.482l.149-.022.841 10.518A2.75 2.75 0 007.596 19h4.807a2.75 2.75 0 002.742-2.53l.841-10.52.149.023a.75.75 0 00.23-1.482A41.03 41.03 0 0014 4.193V3.75A2.75 2.75 0 0011.25 1h-2.5zM10 4c.84 0 1.673.025 2.5.075V3.75c0-.69-.56-1.25-1.25-1.25h-2.5c-.69 0-1.25.56-1.25 1.25v.325C8.327 4.025 9.16 4 10 4zM8.58 7.72a.75.75 0 00-1.5.06l.3 7.5a.75.75 0 101.5-.06l-.3-7.5zm4.34.06a.75.75 0 10-1.5-.06l-.3 7.5a.75.75 0 101.5.06l.3-7.5z" clip-rule="evenodd" />
                     </svg>
