3abcfec120ddbebeb082549c1a75341cd9f24796
Wendryl <wendryl10000@gmail.com>
2022-11-29 19:33:36 -0300

    add fatec tv cidade logos

--- resources/views/partials/rodape_governo.blade.php
+++ resources/views/partials/rodape_governo.blade.php
@@ -1,9 +1,111 @@
 <footer class="rodape">
     <ul class="rodape__social">
-        <li><a href="https://www.proac.sp.gov.br/"><img src="{{ asset('img/proac_imagem.png') }}" alt="icone do PROAC " class='regua_governo'></a></li>
-        <li><a href="https://www.cultura.sp.gov.br/"><img src="{{ asset('img/sec_cultura.png') }}" alt="" class="regua_governo"></a></li>
-        <li><a href="https://www.saopaulo.sp.gov.br/"><img src="{{ asset('img/gov_sp.png') }}" alt="" class="regua_governo"></a></li>
-        <li><a href="https://www.gov.br/turismo/pt-br/secretaria-especial-da-cultura"><img src="{{ asset('img/sec_especial_cultura.png') }}" alt="" class="regua_governo"></a></li>
-        <li><a href="https://www.gov.br/turismo/pt-br"><img src="{{ asset('img/min_turismo.png') }}" alt="" class="regua_governo"></a></li>
-        <li><a href="https://www.gov.br/pt-br"><img src="{{ asset('img/gov_federalpng.png') }}" alt="" class="regua_governo"></a></li>
+        <li>
+            <a href="javascript:void(0)"
+                ><img
+                    src="{{ asset('img/kilometrarte.png') }}"
+                    alt="icone do PROAC "
+                    class="regua_fatec-cidade"
+            /></a>
+        </li>
+        <li>
+            <a href="javascript:void(0)"
+                ><img
+                    src="{{ asset('img/logo_alfaProd.png') }}"
+                    alt="icone do PROAC "
+                    class="regua_fatec-cidade"
+            /></a>
+        </li>
+        <li>
+            <a href="https://fatectaubate.edu.br/"
+                ><img
+                    src="{{ asset('img/logoFatec.png') }}"
+                    alt="icone do PROAC "
+                    class="regua_fatec-cidade"
+            /></a>
+        </li>
+        <li>
+            <a href="https://tvcidade.org.br/"
+                ><img
+                    src="{{ asset('img/logoTvCidade.png') }}"
+                    alt="icone do PROAC "
+                    class="regua_fatec-cidade"
+            /></a>
+        </li>
+        <li>
+            <a href="javascript:void(0)"
+                ><img
+                    src="{{ asset('img/logo_caipiraFilmes.jpg') }}"
+                    alt="icone do PROAC "
+                    class="regua_fatec-cidade"
+            /></a>
+        </li>
+        <li>
+            <a href="javascript:void(0)"
+                ><img
+                    src="{{ asset('img/midia-livre.png') }}"
+                    alt="icone do PROAC "
+                    class="regua_fatec-cidade"
+            /></a>
+        </li>
+        <li>
+            <a href="javascript:void(0)"
+                ><img
+                    src="{{ asset('img/ponto-de-cultura.png') }}"
+                    alt="icone do PROAC "
+                    class="regua_fatec-cidade"
+            /></a>
+        </li>
+    </ul>
+    <ul class="rodape__social">
+        <li>
+            <a href="https://www.proac.sp.gov.br/"
+                ><img
+                    src="{{ asset('img/proac_imagem.png') }}"
+                    alt="icone do PROAC "
+                    class="regua_governo"
+            /></a>
+        </li>
+        <li>
+            <a href="https://www.cultura.sp.gov.br/"
+                ><img
+                    src="{{ asset('img/sec_cultura.png') }}"
+                    alt=""
+                    class="regua_governo"
+            /></a>
+        </li>
+        <li>
+            <a href="https://www.saopaulo.sp.gov.br/"
+                ><img
+                    src="{{ asset('img/gov_sp.png') }}"
+                    alt=""
+                    class="regua_governo"
+            /></a>
+        </li>
+        <li>
+            <a
+                href="https://www.gov.br/turismo/pt-br/secretaria-especial-da-cultura"
+                ><img
+                    src="{{ asset('img/sec_especial_cultura.png') }}"
+                    alt=""
+                    class="regua_governo"
+            /></a>
+        </li>
+        <li>
+            <a href="https://www.gov.br/turismo/pt-br"
+                ><img
+                    src="{{ asset('img/min_turismo.png') }}"
+                    alt=""
+                    class="regua_governo"
+            /></a>
+        </li>
+        <li>
+            <a href="https://www.gov.br/pt-br"
+                ><img
+                    src="{{ asset('img/gov_federalpng.png') }}"
+                    alt=""
+                    class="regua_governo"
+            /></a>
+        </li>
+    </ul>
 </footer>
